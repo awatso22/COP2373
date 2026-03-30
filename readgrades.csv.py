833312552a31feeb6fb0c8347ea51943a289d229
awatso22 <awatso22@student.scf.edu>
2026-03-29 21:22:29 -0400

    read grades csv

---
+++ readgrades.csv.py
@@ -0,0 +1,9 @@
+def read_games():
+    print("\nStudent Grades:\n")
+
+    with open("grades.csv", "r") as file:
+        reader = csv.reader(file)
+
+        # Loop through rows and print formatted output
+        for row in reader:
+            print(f"{row[0]:<15}{row[1]:<15}{row[2]:<10}{row[3]:<10}{row[4]:<10}")
